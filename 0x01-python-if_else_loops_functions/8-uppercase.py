51359106e325eb9a92a886422e0b758356d57c11
R-INYURU <rwema.fred@tutamail.com>
2024-09-11 01:22:47 +0200

    to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,4 +2,5 @@
 def uppercase(str):
     for ch in str:
         val = ord(ch)
-        print("{}".format(chr(val-32) if val > 96 and val < 123 else chr(val)))
+        print("{}".format(chr(val-32) if val > 96 and val < 123 else chr(val)), end='')
+    print()
