fa8fae57b78c3e2f474226cff023f76cc348d45e
R-INYURU <rwema.fred@tutamail.com>
2024-09-13 01:36:24 +0200

    own calc

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    if len(argv) <= 3:
+        print("{} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+    elif argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/':
+        print("Unkown operator. Available operators: +, -, * and /")
+
+    a = int(argv[1])
+    b = int(argv[3])
+    if argv[2] == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif argv[2] == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    if argv[2] == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    if argv[2] == '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
