06355b2e62f46ddc20e1912236a3af4bf1269bbf
R-INYURU <rwema.fred@tutamail.com>
2024-09-14 23:56:59 +0200

    strg formated

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
-        print("{}".format(my_list[i]))
+        num = "{}"
+        print(num.format(my_list[i]))
