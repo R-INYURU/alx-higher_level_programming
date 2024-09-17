7ce222c037c4d5227a38c63987ec3b8e5f350328
R-INYURU <rwema.fred@tutamail.com>
2024-09-17 19:19:54 +0200

    max num

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    return (max(my_list))
+    if len(my_list) == 0:
+        return ("None")
+    mx = 1
+    for i in range(len(my_list)):
+        if my_list[i] > mx:
+            mx = my_list[i]
+    return (mx)
