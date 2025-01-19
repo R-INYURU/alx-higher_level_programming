86718dcba1ccd096afebe20f70208d474dcd27e3
R-INYURU <rwema.fred@tutamail.com>
2025-01-19 15:46:47 +0200

    best_score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dict):
-    max = 0
-    key = ""
-    try:
-        for i in sorted(a_dict):
-            if a_dict[i] > max:
-                max = a_dict[i]
-                key = i
-            else:
-                key = i
-        return (key)
-    except TypeError:
-        return ("None")
+    if not a_dict:
+        return (None)
+
+    return (max(a_dict, key=a_dict.get))
