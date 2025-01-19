c296c8a875832e1c1d426a0fe889e0c09f819213
R-INYURU <rwema.fred@tutamail.com>
2025-01-19 17:55:14 +0200

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in my_list:
-            print("{}".format(i), end="")
+    rem = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            rem += 1
+        except IndexError:
+            break
+    print("")
+    return (rem)
