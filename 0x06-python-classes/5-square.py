f9a0e94b9f4ccf309d14bf2443d118f507c9119b
R-INYURU <rwema.fred@tutamail.com>
2025-01-19 20:53:22 +0200

    pycodestyle

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -31,7 +31,7 @@ class Square:
         Args:
         value (int): size of the square
         """
-        if  type(value) is not int:
+        if type(value) is not int:
             raise TypeError('size must be an integer')
         elif value < 0:
             raise ValueError('size must be >= 0')
