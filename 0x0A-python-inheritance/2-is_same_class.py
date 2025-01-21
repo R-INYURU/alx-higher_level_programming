af37a963652444787870e3fe358f9095494d89b6
R-INYURU <rwema.fred@tutamail.com>
2025-01-21 23:59:46 +0200

    class from

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,5 +4,5 @@
 
 def is_same_class(obj, a_class):
     """ Return True if the object is exactly an instance
-    of the specified class """
+    of the specified clas """
     return (type(obj) == a_class)
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,8 +5,13 @@ Only sub class of
 
 
 def inherits_from(obj, a_class):
-    """ function that returns True if the object is an instance
-    of a class that inherited from the specified class
+    """ func that return True if the object is an instance
+    of a clas that inherited fom the specified clas
+    Args:
+        obj: object that is true
+        a_class: class to inherit fom
+    Return:
+        True if objet is instance
     """
     if type(obj) != a_class:
         return issubclass(type(obj), a_class)
