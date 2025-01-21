707b6fa627e89c964df8b05b6885d1b213c13995
R-INYURU <rwema.fred@tutamail.com>
2025-01-22 00:18:02 +0200

    varname

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,5 +11,5 @@ def read_file(filename=""):
         Exception: when the file can be opened
     """
     with open(filename, 'r', encoding="utf-8") as f:
-        read_file = f.read()
+        read_data = f.read()
         print(read_data, end='')
