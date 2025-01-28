d1b2eadb10030fe6db6578d301641aeec3c4af21
R-INYURU <rwema.fred@tutamail.com>
2025-01-28 17:26:27 +0200

    indentation

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,9 +3,9 @@
 const { argv } = require('process');
 
 function factorial (num) {
-    if (num <= 1) return (1);
+  if (num <= 1) return (1);
 
-    return (factorial(num - 1) * num);
+  return (factorial(num - 1) * num);
 }
 const parsed = parseInt(argv[2]);
 console.log(Number.isNaN(parsed) ? 1 : factorial(parsed));
