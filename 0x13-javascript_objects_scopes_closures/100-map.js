45b0ece4cf87a20cdaf434ede34222d9667a8d89
R-INYURU <rwema.fred@tutamail.com>
2025-03-12 17:51:54 +0200

    semistandard

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 
-const newList = list.map((el,idx) => el * idx);
+const newList = list.map((el, idx) => el * idx);
 console.log(list);
 console.log(newList);
